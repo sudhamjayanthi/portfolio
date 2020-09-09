c55b63a3168a50ee143b2114ad5f7e9139155596
Your name here <workwithsudham@gmail.com>
2020-09-09 09:06:57 +0530

    Updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -53,7 +53,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a Python Developer.", "love everything about code.", "build web-apps", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,

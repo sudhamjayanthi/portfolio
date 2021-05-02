de45538af76e1040b96c28712196503c8f7f5c49
Sudham Jayanthi <workwithsudham@gmail.com>
2021-05-02 20:44:25 +0530

    redesign & optimised

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const writingText = document.getElementById("writing-text");
+const work = document.getElementById("work");
+
+const newProject = (name, img, tag, desc, url) => `<div class="row work-row">
+  <div class="small-12 medium-6 columns">
+    <img src="${img}" alt="project image">
+  </div>
+  <div class="small-12 medium-5 medium-offset-1 columns">
+    <div class="details details-ftm">
+      <h3>${name}</h3>
+      
+      <p class="platform">${tag}</p>
+      <p>${desc}</p>
+      <a class="my-button" rel="noopener" target="_blank" href="${url}">LIVE DEMO</a>
+    </div>
+  </div>
+  </div>`;
+
+const typeAnimation = () => {
+  Typed.new("#writing-text", {
+    strings: [
+      "love to solve problems.",
+      "turn ideas into reality.",
+      "am a Full Stack Developer.",
+    ],
+    typeSpeed: 1,
+    contentType: "text",
+    callback: function () {
+      writingText.style.color = "rgb(71, 71, 71)";
+      writingText.style.background = "rgb(187, 230, 255)";
+    },
+    preStringTyped: function () {},
+    onStringTyped: function () {},
+  });
+};
+
+const renderProjects = (projects) => {
+  projects.forEach((p) => {
+    work.innerHTML += newProject(p.name, p.img, p.tag, p.desc, p.link);
+  });
+  work.innerHTML +=
+    '<p class="text-center"><em>Check out the source code of these projects on <a href="https://github.com/sudhamjayanthi" data-splitbee-event="Social Link" data-splitbee-event-type="github-after-work">my github</a></em></p> ';
+};
+
+typeAnimation();
+renderProjects(PROJDATA);
+
+console.log(
+  "Hey, nice to see your interested to inspect my source code. Feel free to reach out to me on instagram/twitter for any help -- @sudhamjayanthi"
+);
---
+++ js/data.js
@@ -0,0 +1,49 @@
+const PROJDATA = [
+  {
+    name: "Amazon Clone",
+    img: "images/amazon-clone.jpg",
+    tag: "react + firebase",
+    desc:
+      "This is a clone of AMAZON.COM with add to cart,login,checkout & payment with stripe functionality! Made with React.js and Firebase.",
+    link: "https://amazon-clone.sudham.tk/",
+  },
+  {
+    name: "Google Clone",
+    img: "images/google-clone.jpg",
+    tag: "html + vanilla css",
+    desc: "Mobile responsive Google UI Clone using HTML5 and Vanilla CSS3",
+    link: "https://google-clone.sudham.tk",
+  },
+  {
+    name: "My CRUD Blog",
+    img: "images/blog.jpg",
+    tag: "flask - python",
+    desc:
+      "A dynamic blog with CRUD functionality. A fun side project built while learning Flask.",
+    link: "https://myblog.sudham.tk/",
+  },
+  {
+    name: "Simple Shopping List",
+    img: "images/shopping-list.jpg",
+    tag: "flask - python",
+    desc:
+      "A Shopping List web app with Flask. You can add/delete the products & export to excel. Built for my first client.",
+    link: "https://shoplist.sudham.tk/",
+  },
+  {
+    name: "ZeroCastinate",
+    img: "images/zerocastinate.jpg",
+    tag: "react + firebase",
+    desc:
+      "Yet another todo-list app! Built to sharpen my skills. A 5-hour build using React + Firebase(powerful combo )  ",
+    link: "https://todo.sudham.tk/",
+  },
+  {
+    name: "Diwali Greeting Generator",
+    img: "images/diwali-gg.jpg",
+    tag: "flask - python",
+    desc:
+      "This project is made on Diwali for people to generate diwali greetings, just a quick one-day hack ;)",
+    link: "https://diwali-gg.sudham.tk/",
+  },
+];
